f6d8eff57e28b59cb064b94a385c308912e63762
seta7D5 <seta7D5@protonmail.com>
2021-10-13 12:38:31 -0300

    Init

---
+++ README.txt
@@ -0,0 +1,22 @@
+# gcbh
+Simple Shell hack for cloning all branches above the master one.
+
+## Usage
+gcbh -M master_branch -c http://git.example.net/repo.git
+
+## License and copyright
+Created by takusuman and kindly given to Arquivotheca.
+This is licensed under Unlicense, in other words, it's fully 
+free and released into public domain.
+
+## Contribute
+You can make a donation to takusuman using his wallet addresses:
+BTC: 18Xr5vppToewH88v5WaK1Ko4TJ8oWu6mL4
+BCH: qqdlnplp0mcsg80m7n037n8ysn793cvefusea44kza
+XMR:48Ux2DgXigzMzz8uoYLDgpRxtTvnQtAXhSiUwH56twUVUH7tq2dvQ8fRA3jsCxow6SEvuoVv8S8AG4WNjgMrmpPqCfDXme8 
+
+Arquivotheca itself don't intend to recieve any financial
+contributions, just archives and code, but we encourage donations
+for our contributors.
+
+Signature, seta7D5
---
+++ gcbh.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# gcbh - A simple helper for cloning a repo and all its branches
+# Copyright (C) 2021: takusuman; kindly given for Arquivotheca
+# SPDX-License-Identifier: Unlicense
+
+main(){
+  while getopts ":c:M:h" option; do
+    case "$option" in
+      M) export master_branch="$OPTARG" ;;
+      c) clone_all "$OPTARG";;
+      \?|h) print_help ;;
+    esac
+  done
+
+}
+
+clone_all(){
+  repo=$1
+  repodir="$(basename $repo)"
+  printf 'cloning %s (%s) into %s...\n' "$repo" "$master_branch" \
+  "$PWD/$repodir"
+  git clone -v $repo "$repodir" \
+    && cd $repodir 
+    bs=$(git branch -a | grep remotes | grep -v HEAD \
+        | grep -v $master_branch | sort)
+    for i in $bs; do
+      b=$(basename $i)
+      git clone -v -b $b --single-branch $repo $b
+    done
+  return 0
+}
+
+print_help(){
+  printf '[usage]: %s -M master-branch -c http://git.example.net/repo.git' $0
+}
+
+main $@
